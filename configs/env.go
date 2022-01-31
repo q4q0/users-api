e9d549ff2a089d9b7b08aac5640f5859e6a1d327
~/q4q0 <61029571+q4q0@users.noreply.github.com>
2022-01-31 18:52:00 +0300

    establish connection with mongodb

---
+++ configs/env.go
@@ -0,0 +1,16 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func EnvMongoURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("ERROR: can not load .env")
+	}
+	return os.Getenv("MONGOURI")
+}
--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"users-api/configs"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	configs.ConnectDB()
+
+	router.HandleFunc("/status", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(rw).Encode(map[string]string{"statusCode": "200", "success": "true"})
 	}).Methods("GET")
